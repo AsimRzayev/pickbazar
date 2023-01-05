8bb70002ea1bc174ede3165dbe2c14b664b86901
AsimRzayev <asimrr@code.edu.az>
2023-01-05 19:50:02 +0400

    Add pickbazar react template

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {
+  Box,
+} from '@chakra-ui/react';
+
+function App() {
+  return (
+      <Box textAlign="center" fontSize="xl">
+       salam
+      </Box>
+  );
+}
+
+export default App;
